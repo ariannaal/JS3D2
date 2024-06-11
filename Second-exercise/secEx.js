659faa7567c25d18ad1bfba13bf92e18d994608d
ariannaal <ariannaloreti25@gmail.com>
2024-06-11 17:27:26 +0200

    adds seconds

--- Second-exercise/secEx.js
+++ Second-exercise/secEx.js
@@ -1,12 +1,14 @@
-// let counter = 0
+
 
 function increment() {
     counter++
-     sessionStorage.setItem('counter', counter); //salva il nuovo valore in sessionStorage
+    sessionStorage.setItem('counter', counter); //salva il nuovo valore in sessionStorage
+    document.getElementById("seconds").textContent = counter;
 }
 
 // All'avvio della pagina recupera il conto OPPURE inizia da zero se non c'è valore dentro aslla sessionStorage
 let counter = parseInt(sessionStorage.getItem('counter')) || 0;
+
 
 setInterval(function () {
 
@@ -15,13 +17,7 @@ setInterval(function () {
 
 }, 1000);
 
-// function generatediv(obj) {
-//     const div = document.createElement("div");
-//     const seconds = document.getElementById("seconds");
-//     div.className = "secondsDiv";
-//     div.textContent = obj.counter;
-//     seconds.appendChild(div);
-// }
+
 
 
 
